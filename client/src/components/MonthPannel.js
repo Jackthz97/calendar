59d5d43768a03bab773cc1c12c0d25ebfd74b959
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-07-10 15:39:01 -0400

    create hook to get the weekdays of the month

---
+++ client/src/components/MonthPannel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import Month from "../hooks/Month";
+
+export default function MonthPannel() {
+  
+  return (
+  <Grid container>
+    
+  
+  </Grid>
+  );
+}
---
+++ client/src/hooks/Month.js
@@ -0,0 +1,45 @@
+const displayWeek = (day) => {
+  if (day === 0) {
+    return "Sun";
+  } else if (day === 1) {
+    return "Mon";
+  } else if (day === 2) {
+    return "Tue";
+  } else if (day === 3) {
+    return "Wed";
+  } else if (day === 4) {
+    return "Thu";
+  } else if (day === 5) {
+    return "Fri";
+  } return "Sat";
+};
+
+export default function Month(month, year) {
+  const monthChart = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5];
+  let arr = [];
+
+  let i = year - 2000;
+  let j = Math.floor(i / 4);
+  let weekday = 1 + monthChart[ month - 1 ] + 6 + i + j;
+  let remainder = weekday - (Math.floor(weekday / 7) * 7);
+  let counter = remainder;
+  if (remainder > 0) {
+    for (let x = 0; x < remainder; x++) {
+      arr.push("*");
+    }
+  }
+  for (let y = 1; y < 31; y++) {
+    if (counter > 6) {
+      counter = 0;
+    }
+    arr.push(`${displayWeek(counter)} ${y}`);
+    counter ++;
+  }
+  return remainder, arr;
+
+
+// console.log(displayMonth(7, 2022));
+  // return (
+  //   <div>Month</div>
+  // )
+}
