903cc24dc8e92124afb1278ec9413118968cb57d
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-08-02 16:44:26 -0400

    setup backend login check

--- server/app.js
+++ server/app.js
@@ -1,53 +1,34 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const db = require('./configs/db.config');
 const cors = require("cors");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-// db connection
-const db = require("./configs/db.config");
+const app = express();
 
-const { SocketAddress } = require("net");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use('/', indexRouter);
+app.use('/users', usersRouter());
+app.put("/user-data", (req, res) => {
+  let data = {
+    email: req.body.data.email,
+    password: req.body.data.password,
+  };
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter(db));
-
-app.put("/user-login", (req, res) => {
-
-});
-
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  db.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [
+    data.email,
+    data.password,
+  ])
+    .then((response) => res.send(response.rows[0]))
+    .catch((e) => console.error(e.stack));
 });
 
 module.exports = app;
